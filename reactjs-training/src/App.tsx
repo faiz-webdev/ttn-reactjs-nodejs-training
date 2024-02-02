37709a472330d6d43e738e28cb534389b5183434
faizttn <faijuddin@tothenew.com>
2024-02-02 16:08:52 +0530

    display input box value in home page

--- reactjs-training/src/App.tsx
+++ reactjs-training/src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const App: React.FC = () => {
+  const [name, setName] = useState("");
+  const [updateName, setUpdateName] = useState<string>()
+
+  const handleAdd = () => {
+    setUpdateName(name)
+
+  }
+
   return <>
-    <div className="App"><h1>Home</h1></div>
+    <div className="App"><h1>Hello World</h1></div>
+    <div>
+      <input value={name} name='name' type="text" onChange={(e) => { setName(e.target.value) }} />
+      <button onClick={handleAdd}>Add</button>
+    </div>
+    <div>
+      <ul>
+        {updateName && (<li>{updateName}</li>)}
+      </ul>
+    </div>
   </>
 }
 
