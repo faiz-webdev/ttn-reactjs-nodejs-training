4aa128b03e53f945a4544ba7ee271583c25131d2
faizttn <faijuddin@tothenew.com>
2024-02-06 17:43:33 +0530

    convert json to excel completed

--- node-training/routes/excel.route.ts
+++ node-training/routes/excel.route.ts
@@ -2,10 +2,11 @@ import { Request, Response, Router } from "express";
 import * as fs from "fs";
 import * as path from "path";
 import { json2csv } from "json-2-csv";
+import * as XLSX from "xlsx";
 
 const router: Router = Router();
 
-router.get("/create-excel", async (req: Request, res: Response) => {
+router.get("/create-csv", async (req: Request, res: Response) => {
   let response: any;
   const filePath = path.join(__dirname, "../sample.json");
   fs.readFile(filePath, { encoding: "utf-8" }, (err, data) => {
@@ -19,6 +20,41 @@ router.get("/create-excel", async (req: Request, res: Response) => {
       res.send({ message: "Read fileeee", data: writeFilte });
     }
   });
+});
+
+router.get("/create-excel", async (req: Request, res: Response) => {
+  let response: any;
+  let filePath = path.join(__dirname, "../sample.json");
+
+  try {
+    fs.readFile(filePath, { encoding: "utf-8" }, (err, data: any) => {
+      if (err) {
+        response = err;
+        res.send({
+          message: "Error in json to excel" + err.message,
+          data: response.message,
+          success: false,
+        });
+      } else {
+        const josnData = JSON.parse(data);
+        const worksheet = XLSX.utils.json_to_sheet(josnData);
+        const workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+
+        // Write workbook to a file
+        XLSX.writeFile(workbook, "data.xlsx");
+        res.send({
+          message: "Json to excel converted successful",
+          success: true,
+        });
+      }
+    });
+  } catch (error) {
+    res.send({
+      message: error,
+      success: false,
+    });
+  }
 });
 
 export default router;
