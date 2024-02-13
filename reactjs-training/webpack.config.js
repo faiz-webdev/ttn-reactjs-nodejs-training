4c1cddb1e0b8e3a2a7c07920a513696df78f030d
faizttn <faijuddin@tothenew.com>
2024-02-13 19:28:07 +0530

    changes in webpack file

--- reactjs-training/webpack.config.js
+++ reactjs-training/webpack.config.js
@@ -1,45 +1,53 @@
-const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: "./src/index.tsx",
-  outout: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+  entry: "../reactjs-training/src/index.tsx",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "../reactjs-training/build"),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template:"./src/index.html",
-    })
+      template: path.resolve(
+        __dirname,
+        "../reactjs-training/public",
+        "index.html"
+      ),
+      favicon: "../reactjs-training/public/favicon.ico",
+      manifest: path.resolve(
+        __dirname,
+        "../reactjs-training/public",
+        "manifest.json"
+      ),
+    }),
   ],
+  resolve: {
+    modules: [__dirname, "../reactjs-training/src", "node_modules"],
+    extensions: [".js", ".jsx", ".tsx", ".ts"],
+  },
   module: {
     rules: [
       {
-        test: /\.(js|js)x?$/,
+        test: /\.(js|ts)x?$/,
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
       {
-        test: /\.(ts|tsx)$/,
+        test: /\.css$/,
         exclude: /node_modules/,
-        use: ["ts-loader"],
-      },
-      {
-        test: /\.(css|scss)$/,
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        test: /\.(png|svg|jpg|gif)$/,
+        exclude: /node_modules/,
         use: ["file-loader"],
       },
+      {
+        test: /\.(jpeg|ico)$/,
+        exclude: /node_modules/,
+        use: ["file-loader?name=[name].[ext]"], // ?name=[name].[ext] is only necessary to preserve the original file name
+      },
     ],
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-  },
-  devServer: {
-    contentBase: "./dist",
-    port: 3000,
-    open: true,
   },
 };
